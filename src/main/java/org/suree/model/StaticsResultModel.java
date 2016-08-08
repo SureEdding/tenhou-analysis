54105b03ad7538ddaada7297ef62b1f8f1e054fa
Sure <sure@suree.org>
2016-08-08 11:24:08 +0800

    index.html

--- src/main/java/org/suree/model/StaticsResultModel.java
+++ src/main/java/org/suree/model/StaticsResultModel.java
@@ -7,7 +7,76 @@ import java.util.Map;
  */
 public class StaticsResultModel {
 
-    private Map<Integer, Double> yakus;
+    private Map<Integer, Double> yakusRate;
+    private Double riichiRate;
+    private Double fuuruRate;
+    private Double agariRate;
+    private Double jyuRate;
+    private Double avAgariPoints;
+    private Double avjyuPoints;
+    private Double ryukyokuRate;
 
+    public Map<Integer, Double> getYakusRate() {
+        return yakusRate;
+    }
 
+    public void setYakusRate(Map<Integer, Double> yakusRate) {
+        this.yakusRate = yakusRate;
+    }
+
+    public Double getRiichiRate() {
+        return riichiRate;
+    }
+
+    public void setRiichiRate(Double riichiRate) {
+        this.riichiRate = riichiRate;
+    }
+
+    public Double getFuuruRate() {
+        return fuuruRate;
+    }
+
+    public void setFuuruRate(Double fuuruRate) {
+        this.fuuruRate = fuuruRate;
+    }
+
+    public Double getAgariRate() {
+        return agariRate;
+    }
+
+    public void setAgariRate(Double agariRate) {
+        this.agariRate = agariRate;
+    }
+
+    public Double getJyuRate() {
+        return jyuRate;
+    }
+
+    public void setJyuRate(Double jyuRate) {
+        this.jyuRate = jyuRate;
+    }
+
+    public Double getAvAgariPoints() {
+        return avAgariPoints;
+    }
+
+    public void setAvAgariPoints(Double avAgariPoints) {
+        this.avAgariPoints = avAgariPoints;
+    }
+
+    public Double getAvjyuPoints() {
+        return avjyuPoints;
+    }
+
+    public void setAvjyuPoints(Double avjyuPoints) {
+        this.avjyuPoints = avjyuPoints;
+    }
+
+    public Double getRyukyokuRate() {
+        return ryukyokuRate;
+    }
+
+    public void setRyukyokuRate(Double ryukyokuRate) {
+        this.ryukyokuRate = ryukyokuRate;
+    }
 }
